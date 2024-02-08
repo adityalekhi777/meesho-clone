c7fe0b0ba4f66753e31cf0b476c763f449136ed1
Aditya Lekhi <lekhiaditya503@gmail.com>
2024-02-08 18:55:16 +0530

    Improved Styling and refactored codefor navbar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
-import {
-  createBrowserRouter,
-  BrowserRouter,
-  Routes,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import { app } from "./firebaseConfig";
-import "./App.css";
-import Headerlayout from "./components/Headerlayout";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import List from "./pages/List.jsx";
-import Category from "./pages/Category.jsx";
-import PrivateCartRoute from "./pages/PrivateCartRoute";
-import Product from "./pages/Product";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Headerlayout from './components/Headerlayout';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import List from './pages/List.jsx';
+import Category from './pages/Category.jsx';
+import PrivateCartRoute from './pages/PrivateCartRoute';
+import Product from './pages/Product';
 
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Navbar from "./components/Navbar";
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Headerlayout />}>
-      
-//     </Route>
-//   )
-// );
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-      <Route index element={<List />} />
-      <Route path="category/:name" element={<Category />} />
-      <Route path="product/:id" element={<Product />} />
-      <Route path="cart" element={<PrivateCartRoute />} />
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<Signup />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route index element={<List />} />
+          <Route path='category/:name' element={<Category />} />
+          <Route path='product/:id' element={<Product />} />
+          <Route path='cart' element={<PrivateCartRoute />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+        </Routes>
       </BrowserRouter>
       {/* <RouterProvider router={router} /> */}
     </Provider>
